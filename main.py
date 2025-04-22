00e3e53e4371398f9f6f75622ae638a1c4e6e071
Luna-ris <peimonqqq@gmail.com>
2025-04-23 01:08:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-from handlers import router
+from handlers import dp
 from database import init_db
 
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
 
 async def main():
     await init_db()
-    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
