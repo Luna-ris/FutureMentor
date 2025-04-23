cb9fa7efcb280eae7912bf861ae2625ff79eb60d
Luna-ris <peimonqqq@gmail.com>
2025-04-23 19:42:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import asyncio
-from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from handlers import dp
 from database import init_db
+from aiogram import Bot
+import logging
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 bot = Bot(token=BOT_TOKEN)
 
 async def main():
+    logger.info("Инициализация базы данных...")
     await init_db()
+    logger.info("Запуск бота...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
