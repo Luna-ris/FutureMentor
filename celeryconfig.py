69ad65702a7f08caebdd6e19a70e9fbddf7a54e3
Luna-ris <peimonqqq@gmail.com>
2025-04-23 19:45:25 +0300

    Update celeryconfig.py

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,4 +1,8 @@
 from celery.schedules import crontab
+import os
+
+broker_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+result_backend = broker_url
 
 beat_schedule = {
     'send-motivational-messages': {
